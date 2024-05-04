79110bb25122fe2424606fa45db529133362b9f3
Oxana Chizitcaia <creativesouloxana@gmail.com>
2024-05-04 22:07:01 +0200

    Added js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const questionElm = document.querySelector('.question');
+questionElm.textContent =
+  'Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.';
 
+const userAnswer = prompt(questionElm.textContent);
+
+const answerTextElm = document.querySelector('.answer__text');
+answerTextElm.textContent = userAnswer;
+
+const answerElm = document.querySelector('.answer');
+
+if (userAnswer === 'Bell') {
+  answerElm.classList.add('answer--correct');
+} else {
+  answerElm.classList.add('answer--wrong');
+}
