85de7809049cc91dbe3b62f9ecd3ac62c8968d74
Oxana Chizitcaia <creativesouloxana@gmail.com>
2024-05-05 20:55:20 +0200

    Adding .toLowerCase

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const questionElm = document.querySelector('.question');
 questionElm.textContent =
   'Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.';
 
-const userAnswer = prompt(questionElm.textContent);
+const userAnswer = prompt(questionElm.textContent).toLowerCase();
 
 const answerTextElm = document.querySelector('.answer__text');
 answerTextElm.textContent = userAnswer;
 
 const answerElm = document.querySelector('.answer');
 
-if (userAnswer === 'Bell') {
+if (userAnswer === 'bell') {
   answerElm.classList.add('answer--correct');
 } else {
   answerElm.classList.add('answer--wrong');
